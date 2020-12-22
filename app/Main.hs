2e658ce3d110cf9d862f073f127a326f29a4107f
James Hetherington <jamespjh@gmail.com>
2020-12-22 17:58:32 +0000

    Nicer IO setup

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,13 @@ module Main where
 
 import Expenses
 
+preamble = "Advent of code 2020\n\n"++
+       "Week one, task one: Expenses for the Elves\n"
+
+expenseResult = "Expense fix:" ++ show fix2020 ++ "\n"
+
 main :: IO ()
 main =
   putStrLn
   (preamble
-  ++ "Expense fix:" ++ show fix2020 ++ "\n")
-  where
-    preamble =
-      ("Advent of code 2020\n\n"++
-       "Week one, task one: Expenses for the Elves\n")
+  ++ expenseResult)
