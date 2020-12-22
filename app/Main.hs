f0249fd49fb795a8a123c266a5fa05529f6245a6
James Hetherington <jamespjh@gmail.com>
2020-12-22 17:45:02 +0000

    Encapsulate IO noise

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,11 @@ module Main where
 import Expenses
 
 main :: IO ()
-main = fix2020
+main =
+  putStrLn
+  (preamble
+  ++ "Expense fix:" ++ show fix2020 ++ "\n")
+  where
+    preamble =
+      ("Advent of code 2020\n\n"++
+       "Week one, task one: Expenses for the Elves\n")
--- src/Expenses.hs
+++ src/Expenses.hs
@@ -2,5 +2,5 @@ module Expenses
     ( fix2020
     ) where
 
-fix2020 :: IO ()
-fix2020 = putStrLn "fixExpensesForElves"
+fix2020 :: Int
+fix2020 = 1000
